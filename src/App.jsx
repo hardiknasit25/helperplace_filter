169e2ca06d42a7d67b7fa21322c07642a97633d9
hardiknasit25 <hardiknasit670@gmail.com>
2024-06-03 17:56:12 +0530

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./Components/Header/Header";
+
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className=" pr-20 ps-20">
+      <Header/>
+    </div>
   )
 }
 
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,46 @@
+import logo from "../../assets/helperplace_logo.svg";
+
+function Header() {
+  const option = [
+    {
+      name: "JOBS",
+      path: "#"
+    },
+    {
+      name: "CANDIDATES",
+      path: "#"
+    },
+    {
+      name: "AGENCY SERVICES",
+      path: "#"
+    },
+    {
+      name: "NEWS & MORE",
+      path: "#"
+    }
+  ]
+  return (
+    <div>
+      <div className="flex justify-start h-14 w-full mt-5">
+        <div className="w-[10%] h-full flex justify-center items-center">
+          <img src={logo} alt="logo"/>
+        </div>
+
+        <div className="flex justify-center items-center w-[70%]">
+          <ul className="flex justify-start w-full">
+            {option.map((item) => (
+              <li className="ps-10 font-semibold hover:text-[#054a84]" key={item}>{item.name}</li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="flex justify-center items-center w-[20%] gap-3 ">
+          <button className="bg-[#25ae88] h-[43px] w-[110px] rounded-md text-white p-2 font-bold">LOGIN</button>
+          <button className="bg-[#EBBA16] h-[43px] w-[110px] rounded-md text-white p-2 font-bold">REGISTER</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
