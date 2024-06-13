9f132231589b54c93dd65b453275a07d732f5822
hardiknasit25 <hardiknasit670@gmail.com>
2024-06-13 10:20:55 +0530

    clear react-select learning from job

--- src/Components/Jobs/Jobs.jsx
+++ src/Components/Jobs/Jobs.jsx
@@ -1,70 +1,7 @@
-import { useState } from 'react';
-import Select, { components } from 'react-select';
-
 function Jobs() {
-  const lang = [
-    'English', 'Cantonese', 'Mandarin', 'Japanese', 'Arabic',
-    'Filipino', 'Indonesian', 'Hindi', 'Thai'
-  ];
-
-  const options = lang.map((language) => ({
-    value: language,
-    label: language
-  }));
-
-  const [selectedOptions, setSelectedOptions] = useState([]);
-
-  const handleonChange = (selected) => {
-    setSelectedOptions(selected || []);
-  };
-
-  const formatSelectedOptions = (selectedOptions) => {
-    return selectedOptions.map(option => `${option.label}`).join('');
-  };
-
-  const CheckboxOption = ({ children,isSelected, ...props }) => (
-    <components.Option {...props}>
-      <div className='flex justify-start items-center'>
-        <input type="checkbox" checked={isSelected} onChange={() => props.isFocused} className='mr-2 w-4 h-4' />
-        <label>{children}</label>
-      </div>
-    </components.Option>
-  );
-
-  const selectedString = formatSelectedOptions(selectedOptions);
-
   return (
-    <Select
-      value={{ value: selectedString, label: selectedString }}
-      onChange={handleonChange}
-      placeholder="Select an option..." 
-      options={options}
-      isMulti
-      isSearchable
-      components={{ Option: CheckboxOption }}
-      styles={{
-        clearIndicator: (baseStyles) => ({
-          ...baseStyles,
-          color: "grey",
-          cursor: 'pointer'
-        }),
-        multiValueLabel: () => ({
-          background: "transperant",
-          backgroundColor: 'transperant'
-        }),
-        multiValueRemove: () => ({
-          background: "none",
-          display: 'none'
-        }),
-        singleValue: () => ({
-          background: 'transperant'
-        }),
-        multiValue: () => ({
-          background: 'none'
-        })
-      }}
-    />
-  );
+    <div>Jobs</div>
+  )
 }
 
-export default Jobs;
+export default Jobs
